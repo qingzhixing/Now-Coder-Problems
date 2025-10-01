e4dfb68ae419f617dd9731b4c820fd62eceacb91
qingzhixing <qzsqqmail@qq.com>
2025-10-01 14:14:17 +0800

    🤩 [AC] F-小红的排列构造

--- 牛客周赛 Round 111/F-小红的排列构造.cpp
+++ 牛客周赛 Round 111/F-小红的排列构造.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n, k;
@@ -7,17 +8,58 @@ int main()
 {
 	cin >> n >> k;
 
-	if (k < n / 2)
+	// n元环 最多拆 n-1 次
+	if (k > n - 1)
 	{
 		cout << -1 << endl;
+		return 0;
 	}
-	else
+
+	/*
+	 * x元环 最多拆 x-1 次
+	 * 所以 要满足k次操作，必定需要n-k个环，拆 n - (n-k) = k 次
+	 */
+	int ring_count = n - k;
+
+	/*
+	 *	每个环最小大小为 2
+	 */
+	if (ring_count * 2 > n)
 	{
-		for (int i = n; i >= 1; i--)
+		cout << -1 << endl;
+		return 0;
+	}
+
+	vector<int> ring_sizes;
+	vector<int> result;
+	while (ring_count--)
+	{
+		if (ring_count)
 		{
-			cout << i << ' ';
+			ring_sizes.push_back(2);
+		}
+		else
+		{
+			// 最后一个环里放入所有剩余元素
+			ring_sizes.push_back(n - (n - k - 1) * 2);
 		}
 	}
-	// TODO: 骗分
+
+	int pointer = 1;
+	for (auto ring_size : ring_sizes)
+	{
+		for (auto number = pointer + 1; number <= pointer + ring_size - 1; number++)
+		{
+			result.push_back(number);
+		}
+		result.push_back(pointer);
+		pointer += ring_size;
+	}
+
+	for (auto number : result)
+	{
+		cout << number << ' ';
+	}
+	cout << endl;
 	return 0;
 }
