8ff990faea4f0c0a32222e6642bfcc00ece117d4
qingzhixing <qzsqqmail@qq.com>
2025-10-08 14:43:49 +0800

    🤷‍♂️ [AC] D-Digital Pairing

--- 牛客周赛 Round 112/D-Digital Pairing.cpp
+++ 牛客周赛 Round 112/D-Digital Pairing.cpp
@@ -8,7 +8,7 @@ void Solution()
 {
 	cin >> n;
 
-	vector<int> nums(2 * n + 10);
+	vector<long long> nums(2 * n + 10);
 
 	for (int index = 1; index <= 2 * n; index++)
 	{
@@ -16,10 +16,42 @@ void Solution()
 	}
 
 	/*
-	 * 从高位往低位试填
+	 * 试填法贪心: 从高位往低位试填
 	 */
-	vector<bool> disabled(2 * n + 10);
-	// for(int bit=45) TODO:Coding Here
+	long long answer = 0;
+	vector<bool> enabled(2 * n + 10, true);
+	for (int bit = 45; bit >= 0; bit--)
+	{
+		vector<int> enabled_index{};
+		for (int index = 1; index <= 2 * n; index++)
+		{
+			// 当前数字在先前已被排除
+			if (!enabled[index])
+			{
+				continue;
+			}
+
+			// 当前位可选择，则尝试选择
+			if ((nums[index] >> bit) & 1)
+			{
+				enabled_index.push_back(index);
+			}
+		}
+
+		vector<bool> next_enabled(2 * n + 10);
+		// 如果试填结果满足要求则可填
+		if (enabled_index.size() >= n)
+		{
+			answer |= 1LL << bit;
+			for (auto index : enabled_index)
+			{
+				next_enabled[index] = true;
+			}
+			enabled = next_enabled;
+		}
+	}
+
+	cout << answer << endl;
 }
 
 int main()
