94ae0a813bc907b2f6733fb646447aaada276f9a
qingzhixing <qzsqqmail@qq.com>
2025-10-16 16:17:59 +0800

    😴 C-rin和快速迭代 暴力出奇迹

--- 牛客2025秋季算法编程训练联赛1-基础组/C-rin和快速迭代.cpp
+++ 牛客2025秋季算法编程训练联赛1-基础组/C-rin和快速迭代.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 long long n;
 
-long long factor_count(long long num) {}
+long long factor_count(long long num)
+{
+	// printf("Num: %lld\n", num);
+	long long cnt = 0;
+	for (long long i = 1; i <= sqrt(num * 1.0); i++)
+	{
+		if (num % i == 0)
+		{
+			if (i * i != num)
+			{
+				// 两因数不相等：另一个因数不是i
+				cnt += 2;
+			}
+			else
+			{
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
 
 int main()
 {
 	cin >> n;
-	cout << factor_count(n) << endl;
+	long long result = 0;
+	while (n != 2)
+	{
+		n = factor_count(n);
+		result++;
+	}
+	cout << result << endl;
 	return 0;
 }
