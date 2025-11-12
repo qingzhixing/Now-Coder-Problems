eaa56704d568f0eecd32819ea29825d759943a2f
qingzhixing <qzsqqmail@qq.com>
2025-11-12 20:21:46 +0800

    🤬 [这题能WA?] C-牛牛战队的秀场

---
+++ 牛客2025秋季算法编程训练联赛5-基础组/C-牛牛战队的秀场.cpp
@@ -0,0 +1,22 @@
+// WA
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double PI = 3.14159265;
+
+int main()
+{
+	double n, r;
+	int i, j;
+	cin >> n >> r;
+	cin >> i >> j;
+
+	double single_edge_length = 2 * r * sin(PI / n);
+
+	double edges = abs(i - j);
+
+	printf("%.6f\n", edges * single_edge_length);
+
+	return 0;
+}
