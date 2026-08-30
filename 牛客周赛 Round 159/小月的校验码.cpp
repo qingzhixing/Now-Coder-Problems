19774b5dd071f87ad56f83fceb55323f3b9d10c9
qingzhixing <qzsqqmail@qq.com>
2026-08-30 20:41:20 +0800

    📦 NEW: 小月的校验码

--- 牛客周赛 Round 159/小月的校验码.cpp
+++ 牛客周赛 Round 159/小月的校验码.cpp
@@ -2,83 +2,50 @@
 #include <string>
 using namespace std;
 
-// 2^20 < 2e7
-const int MAX_N = 2e7;
+const int MAX_N = 2e5 + 10;
+// 2^20 < 2e6
+const int MAX_M = 2e6;
 
-// 树根为 0
-bool exist[MAX_N];
-
-#define LEFT_CHILD(x) (2 * x + 1)
-#define RIGHT_CHILD(x) (2 * x + 2)
+int a[MAX_N];
+bool exist[MAX_M];
 
 int n, b;
 
-void insert(const string &s)
-{
-	int current_node = 0;
-	int len = s.length();
-	for (int i = 0; i < len; i++)
-	{
-		if (s[i] == '0')
-		{
-			current_node = LEFT_CHILD(current_node);
-		}
-		else
-		{
-			current_node = RIGHT_CHILD(current_node);
-		}
-		exist[current_node] = true;
-	}
-}
+int amount;
+int cnt[32];
 
 int main()
 {
 	cin >> n >> b;
-
-	// 公共根存在
-	exist[0] = true;
 	for (int i = 1; i <= n; i++)
 	{
 		string s;
 		cin >> s;
-		insert(s);
+		a[i] = std::stoi(s, nullptr, 2);
+		exist[a[i]] = true;
 	}
 
-	// 查询每一层
-	// cnt[i] 表示第 i 层产生的 单点差分码 的数量
-	int cnt[30] = {};
-	// amount 表示 总共 差分码 数量
-	int amount = 0;
-	for (int i = 0; i < b; i++)
+	// 对于每一个数字，我们每次更改其每一个 0 为 1 ，若产生的数存在，那么算成功
+	for (int i = 1; i <= n; i++)
 	{
-		// 第 i 层的编号 (2 ^ i - 1) ~ (2 ^ (i + 1) - 2)
-		int left = (1 << i) - 1;
-		int right = (1 << (i + 1)) - 2;
-		for (int id = left; id <= right; id++)
+		for (int digit = 0; digit < b; digit++)
 		{
-			bool have_left = false;
-			bool have_right = false;
-			int left_child = LEFT_CHILD(id);
-			int right_child = RIGHT_CHILD(id);
-			if (left_child < MAX_N && exist[left_child])
+			// 当前位不为 0，跳过
+			if ((a[i] >> digit) & 1)
 			{
-				have_left = true;
+				continue;
 			}
-			if (right_child < MAX_N && exist[right_child])
+			int next = a[i] | (1 << digit);
+			if (exist[next])
 			{
-				have_right = true;
-			}
-			if (have_left && have_right)
-			{
-				cnt[i]++;
+				cnt[digit]++;
 				amount++;
 			}
 		}
 	}
 
-	// 输出
 	cout << amount << endl;
-	for (int i = 0; i < b; i++)
+	for (int i = b - 1; i >= 0; i--)
 	{
 		cout << cnt[i] << ' ';
 	}
