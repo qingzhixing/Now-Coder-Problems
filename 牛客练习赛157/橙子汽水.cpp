69e34018894ddea60c632d6f384a619e8498ab7a
qingzhixing <qzsqqmail@qq.com>
2026-09-18 19:20:57 +0800

    📦 NEW: 牛客练习赛157 不想打了好累，补标AB

---
+++ 牛客练习赛157/橙子汽水.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#error TODO: Uncomplemented.
+
+const int MAX_N = 0;
+const int MAX_M = 0;
+
+void Solution()
+{
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Solution();
+	}
+	return 0;
+}
