203300ce7cb5abd19d0bd1c932b5e3dd0578377c
qingzhixing <qzsqqmail@qq.com>
2026-09-20 09:38:16 +0800

    📦 NEW: 啊_美丽卡洛

--- 牛客练习赛157/啊_美丽卡洛.cpp
+++ 牛客练习赛157/啊_美丽卡洛.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-#error TODO: Uncomplemented.
+const int MAX_N = 2e5 + 10;
 
-const int MAX_N = 0;
-const int MAX_M = 0;
+int n;
+int a[MAX_N];
 
 void Solution()
 {
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+	}
+
+	sort(a + 1, a + 1 + n);
+
+	int x = a[1];
+	int k = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		x = max(x + 1, a[i]);
+		k = max(1.0 * k, ceil((x - a[i]) / 2.0));
+	}
+
+	cout << k << endl;
 }
 
 int main()
